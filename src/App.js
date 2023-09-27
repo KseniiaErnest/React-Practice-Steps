cd0b1d383e9c3183e844a16bf460ba1a49403f9a
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-09-27 13:35:54 -0400

    Complete

--- src/App.js
+++ src/App.js
@@ -7,21 +7,30 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  )
+}
+
+ function Steps() {
  const [step, setStep] = useState(1);
  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if(step > 1) setStep(step - 1);
+    if(step > 1) setStep((s) => s - 1);
   };
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
       }
 
 
   return (
-    <>
-    <butto className='close' onClick={() => setIsOpen(!isOpen)}>&times;</butto>
+    <div>
+    <butto className='close' onClick={() => setIsOpen((is) => !is)}>&times;</butto>
 { isOpen && (
   <div className="steps">
   <div className="numbers">
@@ -38,7 +47,7 @@ export default function App() {
   </div>
 </div>
 )}
-    </>
+    </div>
   )
 }
 
