7680b596ce1d087c1dd427f0c5b5e46dcea607b7
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-10-03 10:22:47 -0400

    Children prop

--- src/App-v1.js
+++ src/App-v1.js
@@ -30,7 +30,7 @@ export default function App() {
 
   return (
     <div>
-    <button className='close' onClick={() => setIsOpen((is) => !is)}>&times;</button>
+    <butto className='close' onClick={() => setIsOpen((is) => !is)}>&times;</butto>
 { isOpen && (
   <div className="steps">
   <div className="numbers">
@@ -39,29 +39,15 @@ export default function App() {
     <div className={step >= 3 ? 'active' : ''}>3</div>
   </div>
 
- <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+  <p className="message">Step {step}: {messages[step - 1]} </p>
 
   <div className="buttons">
-  <Button textColor='#fff' bgColor='#7950f2' onClick={handlePrevious} ><span>👈🏻</span>Previous</Button>
-  <Button textColor='#fff' bgColor='#7950f2' onClick={handleNext} >Next <span>👉🏻</span></Button>
+    <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handlePrevious} >Previous</button>
+    <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handleNext}>Next</button>
   </div>
 </div>
 )}
     </div>
   )
-}
-
-function StepMessage({ step, children }) {
-return (  <div className="message">
-  <h3>Step {step}</h3> 
-  {children}
-  </div>)
-}
-
-function Button( {textColor, bgColor, onClick, children} ) {
-  return  (
-  <button style={{backgroundColor: bgColor, color: textColor}} onClick={onClick}>{children}</button>
-  )
-
 }
 
